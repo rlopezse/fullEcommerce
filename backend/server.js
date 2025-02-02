aac8dc81f715aa6b507bfbaf345adb4c446dc822
rlopezse <rza.lopez.s@gmail.com>
2025-02-02 18:16:56 -0300

    added: primeros pasos para usar express con mysql

---
+++ backend/server.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const compression = require('compression');
+const mysql= require('mysql2');
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const app = express();
+//const PORT = process.env.PORT || 3000;
+const port = 3000;
+
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+
+var connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+console.log('connection', process.env.HOST);
+
+connection.connect((err) => {
+  if (err) {
+    return;
+  }
+});
+
+app.get('/', (req, res) => {
+  res.status(404);
+});
+
+//obtener todos los equipos
+app.get('/equipos', (req, res) => {
+  connection.query('SELECT * FROM phonesData', (err, results) => {
+    if (err) {
+      res.status(500).send('Error executing query');
+      return;
+    } else if(results.length === 0) {
+      res.status(200).send('No data found');
+      return;
+    }
+
+    res.set('Cache-Control', 'max-age=300000, must-revalidate')
+    res.json(results);
+  });
+});
+
+//guadar carrito de compras
+app.post('/store', (req, res) => {
+  res.send('Hello World!');
+});
+
+//obtener carrito de compras
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
