c5c53893d7c7b00ea2fc785982ca5f3b14234930
rlopezse <rza.lopez.s@gmail.com>
2025-02-15 23:05:49 -0300

    updated: variables de entorno y consultas por url

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,22 @@
 require('dotenv').config();
 const compression = require('compression');
-const mysql= require('mysql2');
+const mysql = require('mysql2');
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(cors());
 app.use(helmet());
 app.use(compression());
 
 var connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-});
-
-
-connection.connect((err) => {
-  if (err) {
-    return;
-  }
-  console.log('connection', process.env.HOST);
+  host: '192.168.0.2',
+  user: 'ronaldlopez',
+  password: 'pW1VfcTO8HQ',
+  database: 'fullEcommerce',
 });
 
 app.get('/', (req, res) => {
@@ -32,17 +25,21 @@ app.get('/', (req, res) => {
 
 //obtener todos los equipos
 app.get('/api/equipos', (req, res) => {
-  connection.query('SELECT * FROM phonesData', (err, results) => {
+  if (req.query.year) {
+    var query = `SELECT * FROM phonesData WHERE year='${req.query.year}'`;
+  } else {
+    var query = 'SELECT * FROM phonesData';
+  }
+
+  connection.query(query, (err, results) => {
     if (err) {
       res.status(500).send('Error executing query');
-      return;
-    } else if(results.length === 0) {
+    } else if (results.length === 0) {
       res.status(200).send('No data found');
-      return;
+    } else {
+      res.set('Cache-Control', 'max-age=300000, must-revalidate');
+      res.status(200).json(results);
     }
-
-    res.set('Cache-Control', 'max-age=300000, must-revalidate')
-    res.status(200).json(results);
   });
 });
 
@@ -53,5 +50,5 @@ app.post('/api/store', (req, res) => {
 
 //obtener carrito de compras
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running and listening on port ${port}`);
 });
