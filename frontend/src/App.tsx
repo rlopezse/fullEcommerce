df6fbfc2570e186ba73e8932cff65ac095bab91d
rlopezse <rza.lopez.s@gmail.com>
2025-02-16 18:21:30 -0300

    added: frontend layer

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [equipos, setEquipos] = useState<any[]>([]);
+  useEffect(() => {
+    fetch('http://192.168.0.2/api/equipos')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setEquipos(data);
+      });
+  }, []);
+
+
+  return (
+    <>
+      {equipos.map((equipo) => (
+        <p key={equipo.name}>{equipo.name}</p>
+      ))} 
+    </>
+  )
+}
+
+export default App
